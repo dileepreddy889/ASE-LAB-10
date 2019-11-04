15ccda338d754c089f26b6bb81594b4a6f160198
dileepreddy889 <54335466+dileepreddy889@users.noreply.github.com>
2019-11-03 22:27:39 -0600

    ASE Lab 10

---
+++ Document and code/index.js
@@ -0,0 +1,30 @@
+var reqExpress = require('express');
+var loginCookieParser = require('cookie-parser');
+var pug_exp = reqExpress();
+var bodyParser = require('body-parser')
+pug_exp.use(bodyParser.urlencoded({extended: true}));
+pug_exp.use(bodyParser.json());
+
+pug_exp.set('view engine', 'pug');
+pug_exp.use(reqExpress.static('extras'));
+pug_exp.use(loginCookieParser());
+
+pug_exp.get('/', function(req, res){
+  res.render('loginDet')
+});
+
+pug_exp.post('/saveCookies', function(req, res){
+  console.log("Body");
+  console.log(req.body);
+  res.cookie('loginCookie', req.body);
+  res.render('display_Cookie');
+});
+
+pug_exp.post('/displayDetails', function (req,res) {
+  res.render('display_Details',{loginCookie: req.cookies.loginCookie});
+});
+
+var port = process.env.PORT || 8080;
+pug_exp.listen(port, function(){
+  console.log("Listening to port 8080")
+});
